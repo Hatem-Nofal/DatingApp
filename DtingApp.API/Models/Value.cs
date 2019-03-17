bf229e5ef8cfc5157cde72ed2aa0e2069131bb5b
Hatem Nofal <eng.hatemnofal@gmai.com>
2019-03-17 13:58:49 +0200

    Initial Commet

---
+++ DtingApp.API/Models/Value.cs
@@ -0,0 +1,16 @@
+namespace DtingApp.API.Models
+{
+    public class Value
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        
+    
+
+    }
+
+
+        
+    
+}
