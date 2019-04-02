70e9a1691ec8a78319802960a573674dfa89f610
Hatem Nofal <eng.hatemnofal@gmai.com>
2019-04-02 11:12:57 +0200

    API Changes

--- DtingApp.API/Controllers/AuthController.cs
+++ DtingApp.API/Controllers/AuthController.cs
@@ -42,7 +42,7 @@ namespace DtingApp.API.Controllers {
 
         [HttpPost ("login")]
         public async Task<IActionResult> Login (UserForLoginDto userForLoginDto) {
-            throw new Exception ("Computer says no!");
+           
             var userFromRepo = await _repo.Login (userForLoginDto.Username, userForLoginDto.Password);
             
             if (userFromRepo == null)
--- DtingApp.API/Helpers/Extensions.cs
+++ DtingApp.API/Helpers/Extensions.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Http;
-namespace DtingApp.API.Helpers
-{
-    public static class Extensions
-    {
-        public static void AddApplicationError(this HttpResponse respone , string message)
-        {
+namespace DtingApp.API.Helpers {
+    public static class Extensions {
+        public static void AddApplicationError (this HttpResponse respone, string message) {
 
-respone.Headers.Add("Application-Error",message);
-respone.Headers.Add("Access-control-Expose-Header","Application-Error");
-respone.Headers.Add("Access-control-Allow-Origin","*");
+            respone.Headers.Add ("Application-Error", message);
+            respone.Headers.Add ("Access-control-Expose-Header", "Application-Error");
+            respone.Headers.Add ("Access-control-Allow-Origin", "*");
 
         }
     }
